cb8984cc66b8f5ceba7c9d1483f3a6d5dfbb977b
fluidmotionstudios <fluidmotionstudios@outlook.com>
2016-01-25 21:36:58 -0500

    Added collection for profileImages

--- client/templates/navbar.js
+++ client/templates/navbar.js
@@ -1,3 +1,9 @@
 Template.navbar.rendered = function() {
   $(".button-collapse").sideNav();
 };
+
+Template.navbar.events({
+  'click #logout': function(event){
+    AccountsTemplates.logout();
+  }
+});
